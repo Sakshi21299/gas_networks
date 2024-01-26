f0549ec6e9c6fc04d3a2920ce3d7b8d34e78b6c8
Sakshi <73687517+Sakshi21299@users.noreply.github.com>
2024-01-26 10:50:54 -0500

    Create setup.py
    Add setup file to make it a package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="gas_networks",
+    description=(
+        "Models and algorithms for optimization of gan network problems"
+    ),
+    author="Sakshi Naik and Lavinia Ghilardi",
+    packages=["gas_net"],
+)
