5c093da9eaef23b827f5d4428c74a54aeee32e09
Sakshi <73687517+Sakshi21299@users.noreply.github.com>
2024-04-29 13:46:11 -0400

    add epsilon argument

--- gas_net/util/make_demand_dynamic.py
+++ gas_net/util/make_demand_dynamic.py
@@ -8,10 +8,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyomo.environ as pyo
 
-def dynamic_demand_profile(ss_demand):
+def dynamic_demand_profile(ss_demand, epsilon = 0):
     time_length = len(ss_demand)
     x = np.linspace(0, 2*np.pi, time_length)
-    dynamic_demand = ss_demand + ss_demand*np.sin(x)/20
+    dynamic_demand = ss_demand + ss_demand*np.sin(x)/20 + epsilon*np.sin(x)/20
     return dynamic_demand
 
 def dynamic_demand_profile_extended(dynamic_demand):
@@ -22,7 +22,7 @@ def dynamic_demand_profile_extended(dynamic_demand):
     return dynamic_demand_extended
     
     
-def dynamic_demand_calculation(m, time_length = None, extended_profile = False):
+def dynamic_demand_calculation(m, time_length = None, extended_profile = False, epsilon = 0):
     if time_length is None:
         time_length = len(m.Times)
         
@@ -30,7 +30,7 @@ def dynamic_demand_calculation(m, time_length = None, extended_profile = False):
     for s in m.wCons:
         if s[0].startswith('sink'):
             ss_demand = [pyo.value(m.wCons[s])]*time_length
-            dynamic_demand[s[0]] = dynamic_demand_profile(ss_demand)
+            dynamic_demand[s[0]] = dynamic_demand_profile(ss_demand, epsilon = epsilon)
             if extended_profile:
                 dynamic_demand[s[0]] = dynamic_demand_profile_extended(dynamic_demand[s[0]])
     return dynamic_demand
@@ -39,7 +39,9 @@ def dynamic_demand_calculation(m, time_length = None, extended_profile = False):
             
 if __name__ == "__main__":
     ss_demand = [16]*25
-    dynamic_demand = dynamic_demand_profile(ss_demand)
+    dynamic_demand = dynamic_demand_profile(ss_demand, epsilon = 0)
+    plt.plot(dynamic_demand)
+    dynamic_demand = dynamic_demand_profile(ss_demand, epsilon = 1)
     plt.plot(dynamic_demand)
     plt.plot(ss_demand)
     
