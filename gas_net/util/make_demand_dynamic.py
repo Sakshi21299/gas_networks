422feabd8b37d5a879d439412d8e7878ce9be951
Sakshi <73687517+Sakshi21299@users.noreply.github.com>
2024-11-04 09:15:19 -0500

    make dynamic demand more flexible

--- gas_net/util/make_demand_dynamic.py
+++ gas_net/util/make_demand_dynamic.py
@@ -8,10 +8,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyomo.environ as pyo
 
-def dynamic_demand_profile(ss_demand, epsilon = 0):
+def dynamic_demand_profile(ss_demand, num_time_periods = 1, epsilon = 0):
+    #amplitude = 1/20 #Original amplitude set for all results
+    amplitude = 1/20
+    
     time_length = len(ss_demand)
-    x = np.linspace(0, 2*np.pi, time_length)
-    dynamic_demand = ss_demand + ss_demand*np.sin(x)/20 + epsilon*np.sin(x)/20
+    x = np.linspace(0, 2* num_time_periods*np.pi, time_length)
+    dynamic_demand = ss_demand + ss_demand*np.sin(x)*amplitude + epsilon*np.sin(x)*amplitude
 
     return dynamic_demand
 
@@ -23,7 +26,7 @@ def dynamic_demand_profile_extended(dynamic_demand):
     return dynamic_demand_extended
     
     
-def dynamic_demand_calculation(m, time_length = None, extended_profile = False, epsilon = 0):
+def dynamic_demand_calculation(m, num_time_periods = 1, time_length = None, extended_profile = False, epsilon = 0):
     if time_length is None:
         time_length = len(m.Times)
         
@@ -31,7 +34,7 @@ def dynamic_demand_calculation(m, time_length = None, extended_profile = False, 
     for s in m.wCons:
         if s[0].startswith('sink'):
             ss_demand = [pyo.value(m.wCons[s])]*time_length
-            dynamic_demand[s[0]] = dynamic_demand_profile(ss_demand, epsilon = epsilon)
+            dynamic_demand[s[0]] = dynamic_demand_profile(ss_demand,  num_time_periods, epsilon = epsilon)
             if extended_profile:
                 dynamic_demand[s[0]] = dynamic_demand_profile_extended(dynamic_demand[s[0]])
     return dynamic_demand
@@ -51,16 +54,18 @@ def uncertain_demand_calculation(m, dynamic_demand, uncertainty={(0,13): -0.1, (
                 
          
 if __name__ == "__main__":
-    ss_demand = [16]*25
-    dynamic_demand = dynamic_demand_profile(ss_demand, epsilon = 0)
+    ss_demand = [16.35417]*72
+    dynamic_demand = dynamic_demand_profile(ss_demand,num_time_periods = 3, epsilon = 0)
+    print(dynamic_demand)
     plt.plot(dynamic_demand)
-    dynamic_demand = dynamic_demand_profile(ss_demand, epsilon = 1)
-    plt.plot(dynamic_demand)
-    plt.plot(ss_demand)
+    # dynamic_demand = dynamic_demand_profile(ss_demand, num_time_periods=3, epsilon = 1)
+    # plt.plot(dynamic_demand)
+    # plt.plot(ss_demand)
     
     #Extended demand profile
     plt.figure()
-    ss_demand = [16]*50
     dynamic_demand_extended = dynamic_demand_profile_extended(dynamic_demand)
     plt.plot(dynamic_demand_extended)
-    plt.plot(ss_demand)
+    plt.xlabel('Time(hrs)')
+    plt.ylabel('Gas demand at each node (kg/s)')
+    #plt.plot(ss_demand)
